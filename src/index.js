9b3223585e138997abdd119ba6f4ccdc795ad26b
danielsilvayanez <daniel.silva@xing.com>
2020-12-08 18:08:22 +0100

    Delete unused css files and add AppGrid

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +13,7 @@ ReactDOM.render(
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
