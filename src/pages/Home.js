f104c9c985590a92af8822f4c2ae1ab51bb6c11b
sinafischer <sina.h.fischer@gmail.com>
2020-12-11 15:27:26 +0100

    Get sum of meals and boxes from json file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import deliveries from '../defaultData.json';
 
 export default function Home() {
-  return <div>Test</div>;
+  const reducer = (a, b) => a + b;
+
+  const dayMealDailyTotal = deliveries
+    .map((delivery) => delivery.dayMeal)
+    .reduce(reducer);
+
+  const weekMealDailyTotal = deliveries
+    .map((delivery) => delivery.weekMeal)
+    .reduce(reducer);
+
+  const boxDailyTotal = deliveries
+    .map((delivery) => delivery.box)
+    .reduce(reducer);
+
+  return (
+    <section>
+      <p>Dashboard</p>
+      <p>Tagesgericht gesamt: {dayMealDailyTotal}</p>
+      <p>Wochengericht gesamt: {weekMealDailyTotal}</p>
+      <p>Pfandboxen zurück: {boxDailyTotal}</p>
+    </section>
+  );
 }
