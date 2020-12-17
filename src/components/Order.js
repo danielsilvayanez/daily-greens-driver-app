1d044da2f2cec1d4fe222b4b733327406a61f77e
danielsilvayanez <daniel.silva@xing.com>
2020-12-17 19:25:46 +0100

    Add buttons, colors and hide logic

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,33 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export default function Order({ delivery }) {
-  const [details, setDetails] = useState(false)
+  const [details, setDetails] = useState(false);
+  const [state, setState] = useState(true);
+
   useEffect(() => {
-    console.log(details)
-  }, [details])
+    console.log(details);
+  }, [details]);
+
   return (
     <>
-      {details ? (
-        <Container
-          height="120"
-          background="var(--secondaryBGPurple)"
-          onClick={() => setDetails(!details)}
-        >
-          <div>{delivery.address.name}</div>
-          <div>{delivery.address.street}</div>
-          <div>Tagesessen: {delivery.dayMeal}</div>
-          <div>Wochenessen: {delivery.weekMeal}</div>
-        </Container>
+      {!delivery.done ? (
+        <StyledDiv>
+          {details ? (
+            <Container
+              height="120"
+              background="var(--secondaryBGPurple)"
+              onClick={() => setDetails(!details)}
+            >
+              <div>{delivery.address.name}</div>
+              <div>{delivery.address.street}</div>
+              <div>Tagesessen: {delivery.dayMeal}</div>
+              <div>Wochenessen: {delivery.weekMeal}</div>
+              <StyledButton
+                primary={state}
+                onClick={() => {
+                  JSON.parse((delivery.start = true));
+                  setState(false);
+                }}
+              >
+                start
+              </StyledButton>
+              <StyledButton
+                primary
+                onClick={() => {
+                  alert("Biste sicher?");
+                  JSON.parse((delivery.done = true));
+                }}
+              >
+                done
+              </StyledButton>
+            </Container>
+          ) : (
+            <Container onClick={() => setDetails(!details)}>
+              <div>{delivery.address.name}</div>
+              <div>{delivery.address.street}</div>
+            </Container>
+          )}
+        </StyledDiv>
       ) : (
-        <Container onClick={() => setDetails(!details)}>
-          <div>{delivery.address.name}</div>
-          <div>{delivery.address.street}</div>
-        </Container>
+        console.log("Auftrag erledigt")
       )}
     </>
-  )
+  );
 }
+const StyledDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+  border: 1px solid var(--primaryBGBtnGreen);
+  margin: 10px;
+`;
 
 const Container = styled.div`
   display: grid;
@@ -36,8 +70,14 @@ const Container = styled.div`
   width: 100%;
   justify-content: space-around;
   align-items: center;
-  border-bottom: solid 2px var(--primaryBGBtnGreen);
   > * {
     text-align: center;
   }
-`
+`;
+
+const StyledButton = styled.button`
+  height: 50px;
+  background-color: ${(props) => (props.primary ? "white" : "green")};
+  border-radius: 5px;
+  }
+`;
