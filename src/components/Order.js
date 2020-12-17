19d36f031dacfd0c950110175aa75044c3ad0de0
Paul Van Houtem <paulvanhoutem@Pauls-iMac.fritz.box>
2020-12-17 14:37:45 +0100

    Added css to Order.js

--- src/components/Order.js
+++ src/components/Order.js
@@ -9,16 +9,20 @@ export default function Order({ delivery }) {
   return (
     <>
       {details ? (
-        <Container height="120" onClick={() => setDetails(!details)}>
-          <Name>{delivery.address.name}</Name>
-          <Address>{delivery.address.street}</Address>
+        <Container
+          height="120"
+          background="var(--secondaryBGPurple)"
+          onClick={() => setDetails(!details)}
+        >
+          <div>{delivery.address.name}</div>
+          <div>{delivery.address.street}</div>
           <div>Tagesessen: {delivery.dayMeal}</div>
           <div>Wochenessen: {delivery.weekMeal}</div>
         </Container>
       ) : (
         <Container onClick={() => setDetails(!details)}>
-          <Name>{delivery.address.name}</Name>
-          <Address>{delivery.address.street}</Address>
+          <div>{delivery.address.name}</div>
+          <div>{delivery.address.street}</div>
         </Container>
       )}
     </>
@@ -26,12 +30,16 @@ export default function Order({ delivery }) {
 }
 
 const Container = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
   height: ${(props) => props.height || 60}px;
-  width: 80%;
+  width: 100%;
   justify-content: space-around;
   align-items: center;
-  border: solid 1px #000;
+  border-bottom: solid 2px var(--primaryBGBtnGreen);
+  > * {
+    text-align: center;
+  }
 `
 
 const Name = styled.div``
