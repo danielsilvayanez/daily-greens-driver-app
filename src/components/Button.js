04c67c66a4099b5330b5e52360c21eb585f25bf2
danielsilvayanez <daniel.silva@xing.com>
2021-01-07 13:00:42 +0100

    add done and start btn logic

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-export default function Button({ onClick, btnName, btnState = true }) {
+export default function Button({
+  onClick,
+  btnName,
+  disableState,
+  btnState = true,
+}) {
   return (
     <StyledButton
+      disabled={disableState}
       onClick={() => {
-        onClick()
+        onClick();
         // setState(!state);
       }}
       primary={btnState}
     >
       {btnName}
     </StyledButton>
-  )
+  );
 }
 
 const StyledButton = styled.button`
   height: 50px;
-  background-color: ${(props) => (props.primary ? 'white' : 'green')};
+  background-color: ${(props) => (props.primary ? "white" : "green")};
   border-radius: 5px;
-`
+`;
