999abe19f49accc08c3a04992688b45d2b33da29
danielsilvayanez <daniel.silva@xing.com>
2021-03-12 11:33:29 +0100

    Update app driver id in config

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -3,7 +3,6 @@ import firebase from "firebase/app";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://daily-greens-12808.firebaseio.com",
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEGING_SENDER_ID,
